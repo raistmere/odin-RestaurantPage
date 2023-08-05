efa8dd6c87884e42dab826e678a781ae98248c8d
raistmere <raistmere@gmail.com>
2023-08-05 18:59:10 -0500

    Menu page html/css is complete
    Completed the menu html/css content for the page.

--- src/menuLoad.js
+++ src/menuLoad.js
@@ -11,20 +11,15 @@ export default function()
     menuHeader.classList.add("menuHeader");
     menuHeader.textContent = "Welcome to our space menu!";
     this.appendChild(menuHeader);
+    
+    //              \\
+    // Pizza Catagory\\
+    //                \\
 
-    // Menu Catagory Containers\\
-    // Creates a header for the pizza catagory
+    // Creates a container for the pizza catagory
     const pizzaContainer = document.createElement("div");
     pizzaContainer.classList.add("pizzaContainer");
     this.appendChild(pizzaContainer);
-    // Creates a header for the wings catagory
-    const wingsContainer = document.createElement("div");
-    this.appendChild(wingsContainer);
-    // Creates a header for the desserts catagory
-    const dessertContainer = document.createElement("div");
-    this.appendChild(dessertContainer);
-
-    // Pizza Catagory\\
     // Pizza header element
     const pizzaHeader = document.createElement("div");
     pizzaHeader.classList.add("pizzaHeader");
@@ -34,14 +29,110 @@ export default function()
     const pizzaTypes = document.createElement("div");
     pizzaTypes.classList.add("pizzaTypes");
     pizzaContainer.appendChild(pizzaTypes);
-    //Pizza type 1 element
+
+    //Pizza 1 container\\
     const pizza1 = document.createElement("div");
     pizza1.classList.add("pizza");
-    pizza1.textContent = "Pizza 1";
     pizzaTypes.appendChild(pizza1);
-    //Pizza type 2 element
+    // Pizza 1 title
+    const pizza1Title = document.createElement("div");
+    pizza1Title.classList.add("pizzaTitle");
+    pizza1Title.textContent = "Spacer-roni Pizza"
+    pizza1.appendChild(pizza1Title);
+    // Pizza 1 description
+    const pizza1Text = document.createElement("div");
+    pizza1Text.classList.add("pizzaText");
+    pizza1Text.textContent = "Our traditional pepperoni pizza topped with space quality pepperoni that is out of this world!"
+    pizza1.appendChild(pizza1Text);
+
+    //Pizza type 2 container\\
     const pizza2 = document.createElement("div");
     pizza2.classList.add("pizza");
-    pizza2.textContent = "Pizza 2";
     pizzaTypes.appendChild(pizza2);
+    // Pizza 2 title
+    const pizza2Title = document.createElement("div");
+    pizza2Title.classList.add("pizzaTitle");
+    pizza2Title.textContent = "Cheezy Asteriod Pizza"
+    pizza2.appendChild(pizza2Title);
+    // Pizza 2 description
+    const pizza2Text = document.createElement("div");
+    pizza2Text.classList.add("pizzaText");
+    pizza2Text.textContent = "This asteriod has so much cheese that an explosion would be cheeeeezy"
+    pizza2.appendChild(pizza2Text);
+
+    //Pizza type 3 container\\
+    const pizza3 = document.createElement("div");
+    pizza3.classList.add("pizza");
+    pizzaTypes.appendChild(pizza3);
+    // Pizza 3 title
+    const pizza3Title = document.createElement("div");
+    pizza3Title.classList.add("pizzaTitle");
+    pizza3Title.textContent = "The Enterprise"
+    pizza3.appendChild(pizza3Title);
+    // Pizza 3 description
+    const pizza3Text = document.createElement("div");
+    pizza3Text.classList.add("pizzaText");
+    pizza3Text.textContent = "This pizza has everything you need! The flagship of the Pizza Station. It makes other supreme pizzas look like pepperon pizzas"
+    pizza3.appendChild(pizza3Text);
+
+    // WINGS MENU \\
+
+    // Creates a header for the wings catagory
+    const wingsContainer = document.createElement("div");
+    wingsContainer.classList.add("menuTypeContainer");
+    this.appendChild(wingsContainer);
+    // Wings header element
+    const wingsHeader = document.createElement("div");
+    wingsHeader.classList.add("menuTypeHeader");
+    wingsHeader.textContent = "Wings";
+    wingsContainer.appendChild(wingsHeader);
+    // Pizza types container
+    const wingsTypes = document.createElement("div");
+    wingsTypes.classList.add("itemTypes");
+    wingsContainer.appendChild(wingsTypes);
+
+    //Wings 1 container\\
+    const wings1 = document.createElement("div");
+    wings1.classList.add("items");
+    wingsTypes.appendChild(wings1);
+    // Wings 1 title
+    const wings1Title = document.createElement("div");
+    wings1Title.classList.add("itemTitle");
+    wings1Title.textContent = "Gravity Wings"
+    wings1.appendChild(wings1Title);
+    // Wings 1 description
+    const wings1Text = document.createElement("div");
+    wings1Text.classList.add("itemText");
+    wings1Text.textContent = "The ol'fashion wings. No sauce just the taste of pure chicken....in space."
+    wings1.appendChild(wings1Text);
+
+    //Wings 2 container\\
+    const wings2 = document.createElement("div");
+    wings2.classList.add("items");
+    wingsTypes.appendChild(wings2);
+    // Wings 2 title
+    const wings2Title = document.createElement("div");
+    wings2Title.classList.add("itemTitle");
+    wings2Title.textContent = "Space Cowboy Wings"
+    wings2.appendChild(wings2Title);
+    // Wings 2 description
+    const wings2Text = document.createElement("div");
+    wings2Text.classList.add("itemText");
+    wings2Text.textContent = "Smothered with quality bbq sauce and only for the best. Just for you space cowboy!"
+    wings2.appendChild(wings2Text);
+
+    //Wings 3 container\\
+    const wings3 = document.createElement("div");
+    wings3.classList.add("items");
+    wingsTypes.appendChild(wings3);
+    // Wings 3 title
+    const wings3Title = document.createElement("div");
+    wings3Title.classList.add("itemTitle");
+    wings3Title.textContent = "Supernova Wings"
+    wings3.appendChild(wings3Title);
+    // Wings 3 description
+    const wings3Text = document.createElement("div");
+    wings3Text.classList.add("itemText");
+    wings3Text.textContent = "Sooooo hot that it makes your mouth explode with goodness and suffering....literally"
+    wings3.appendChild(wings3Text);
 }
